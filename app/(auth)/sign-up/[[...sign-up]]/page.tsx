8b68b0351bd0449890004d7c96b14953ebd73d6c
Eleeey <ezralaakasen@gmail.com>
2025-05-19 23:26:16 +0100

    1

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,5 +2,5 @@ import { SignIn } from "@clerk/nextjs";
 import { SignUp } from "@clerk/nextjs";
 
 export default function page() {
-  return <SignUp afterSignUpUrl{process.env.SIGNIN_URL}/>;
+  return <SignUp afterSignUpUrl={process.env.SIGNIN_URL}/>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Menance",
+  title: "TrueVest",
   description: "Full fledged investment app",
 };
 
